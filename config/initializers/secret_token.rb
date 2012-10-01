7854deb658362201f5a036088873942067e041eb
Lukas Elmer <lukas.elmer@gmail.com>
2012-10-01 17:25:09 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlanningRenuoCh::Application.config.secret_token = '00b4d99327bb6225dd4d059109b53e72ec0132e5720995295f7412126766fcd76897cab6c3bac94db115ea508140c1af21041d9414af150cbe44a84b32e84c90'
