e2815922d8e5d4da4ea173620eb8c62ccd07e0b3
Lukas Elmer <lukas.elmer@gmail.com>
2012-10-08 01:37:01 +0300

    Update app/helpers/home_helper.rb

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,13 @@
 module HomeHelper
+  def item_name item
+    return item.name if item.is_a? Milestone
+    item.user
+  end
+  def item_kw item
+    return item.when.cweek
+  end
+  def item_hours item
+    return -item.hours if item.is_a? Milestone
+    item.hours
+  end
 end
