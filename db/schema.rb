1ed1cd56a00c87cf6ad3a127fcdab9c3605ac45b
Lukas Elmer <lukas.elmer@gmail.com>
2012-10-09 10:29:41 +0200

    enhancements...

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121007194646) do
+ActiveRecord::Schema.define(:version => 20121009080124) do
 
   create_table "milestones", :force => true do |t|
     t.string   "name",       :null => false
@@ -26,12 +26,10 @@ ActiveRecord::Schema.define(:version => 20121007194646) do
     t.decimal  "hours",      :precision => 4, :scale => 2, :null => false
     t.date     "when",                                     :null => false
     t.integer  "user_id",                                  :null => false
-    t.integer  "project_id",                               :null => false
     t.datetime "created_at",                               :null => false
     t.datetime "updated_at",                               :null => false
   end
 
-  add_index "planned_times", ["project_id"], :name => "index_planned_times_on_project_id"
   add_index "planned_times", ["user_id"], :name => "index_planned_times_on_user_id"
 
   create_table "projects", :force => true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,34 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-[["Lukas", 10], ["Diego", 10], ["Ueli", 10]].each do |name, overhead|
+User.delete_all
+Project.delete_all
+Milestone.delete_all
+PlannedTime.delete_all
+
+[["Lukas", 10], ["Peter", 10], ["Mark", 10]].each do |name, overhead|
   User.create!(name: name, overhead: overhead)
+end
+users = {}
+User.all.each { |v| users[v.name] = v }
+
+%w(ProjectA ProjectB ProjectC).each { |v| Project.create!(name: v) }
+
+
+[
+    ["Lukas", 1, 15], ["Lukas", 2, 20], ["Lukas", 3, 14], ["Lukas", 4, 10], ["Lukas", 5, 10],
+    ["Peter", 1, 10], ["Peter", 2, 18], ["Peter", 3, 14], ["Peter", 4, 12], ["Peter", 5, 10],
+    ["Mark", 1, 12], ["Mark", 2, 18], ["Mark", 3, 14], ["Mark", 4, 15], ["Mark", 5, 10],
+].each do |user_name, kw_offset, hours|
+  PlannedTime.create!(user: users[user_name], when: Time.now + kw_offset.days, hours: hours)
+end
+
+projects = Project.all
+[
+    [0, "SP1", 2, 25], [0, "SP2", 4, 65.5],
+    [1, "Deployment", 3, 28.5],
+    [2, "Deployment", 5, 50],
+
+].each do |project_index, name, kw_offset, hours|
+  Milestone.create!(project: projects[project_index], name: name, when: Time.now + kw_offset.days, hours: hours)
 end
