fea728e51a0c575764818f340f63c0fb0cf81088
Lukas Elmer <lukas.elmer@gmail.com>
2012-11-04 14:36:18 +0100

    added redmine link

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,13 +3,28 @@ module HomeHelper
     return item.user if item.is_a? PlannedTime
     item.name
   end
+
   def item_kw item
     item.when.cweek
   end
+
+  def kw_start_date item
+    date_for_kw item.when, 1
+  end
+
+  def kw_end_date item
+    date_for_kw item.when, 7
+  end
+
+  def date_for_kw date, weekday
+    Date.commercial( d.year, d.cweek, weekday ).strftime( "%y-%m-%d" )
+  end
+
   def item_hours item
     return -item.hours.to_i if item.is_a? Milestone
     item.estimated_or_achieved_hours_without_overhead
   end
+
   def milestone_color time
     return :success if time > -2.0
     return :error if time < -15.0
